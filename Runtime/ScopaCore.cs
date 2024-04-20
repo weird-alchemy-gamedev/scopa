cb2d860edca55fe73e13dbfe763ca45471743436
The Alchemist <37559078+Alchemical-Creations@users.noreply.github.com>
2024-04-20 16:26:23 -0400

    Still doing debug printing

--- Runtime/ScopaCore.cs
+++ Runtime/ScopaCore.cs
@@ -196,7 +196,7 @@ namespace Scopa {
                     face.VAxis = direction == ScopaMesh.Axis.Y ? -System.Numerics.Vector3.UnitZ : -System.Numerics.Vector3.UnitY;
 
                     face.TextureName = face.TextureName.ToLowerInvariant();
-
+                    Debug.Log($"Found face with texture {face.TextureName}")
                     // var center = face.Vertices.Aggregate(System.Numerics.Vector3.Zero, (x, y) => x + y) / face.Vertices.Count;
                     // Debug.DrawRay(center.ToUnity() * config.scalingFactor, face.Plane.Normal.ToUnity(), Color.yellow, 120f, false);
                     
